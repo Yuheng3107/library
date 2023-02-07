968c7c3f699443aadaf43d5764a9398b9d121d6a
Kuang Yu Heng <yuheng3107@gmail.com>
2023-02-07 09:42:39 +0800

    Added css styling

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const submitBtn = document.querySelector('#submit');
+const addBookForm = document.querySelector('#addBook');
+const newBookBtn = document.querySelector('.btn');
+const cancelBtn = document.querySelector('#cancel');
+const overlay = document.querySelector('#overlay');
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    
 }
 
 Book.prototype.info = function () {
@@ -18,7 +23,40 @@ function addBookToLibrary(book) {
 
 function displayBooks(library) {
     for (let book of library) {
+        // TODO
         document.createElement('div');
         
     }
 }
+
+submitBtn.addEventListener('click', (e) => {
+    
+    
+    if (addBookForm.checkValidity()) {
+        addBookForm.style.display = "none";
+        overlay.classList.toggle('overlay');
+    }
+    addBookForm.reportValidity()
+    e.preventDefault();
+    let elements = addBookForm.elements;
+    let title = elements.title.value;
+    elements.title.value = "";
+    let author = elements.author.value;
+    elements.author.value = "";
+    let pages = elements.pages.value;
+    elements.pages.value = "";
+    let read = elements.read.value;
+    let book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+});
+
+newBookBtn.addEventListener('click', (e) => {
+    addBookForm.style.display = 'flex';
+    overlay.classList.toggle('overlay');
+});
+
+cancelBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookForm.style.display = "none";
+    overlay.classList.toggle('overlay');
+})
